8d4ccbb36fb4329379f9bfecc6a7f682533534b9
daoleqh <62601376+daoleqh@users.noreply.github.com>
2021-05-21 21:53:56 +0700

    fix

--- src/components/pages/Admin/index.js
+++ src/components/pages/Admin/index.js
@@ -15,51 +15,6 @@ const index = (props) => {
 
                 <div id="content-wrapper"  style={{with:'100%',marginTop:'93px', marginRight:'20px', marginLeft:'20px'}}>
                     <HomeAdmin></HomeAdmin>
-                    {/* <div className="container-fluid">
-                    <BreadCrumb breadcrumb="" link_root="/admin"></BreadCrumb>
-                        <a style={{cursor:'pointer'}} onClick={()=>props.history.push('/admin/books')}  className="card-link" >
-                            <div >
-                                <div className="card text-white bg-primary o-hidden h-100">
-                                    <div className="card-body">
-                                        <div className="card-body-icon">
-                                            <i className="fas fa-fw fa-book"></i>
-                                        </div>
-                                        <div className="mr-5">
-                                            <a>Quản lý sách</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                        <a style={{cursor:'pointer'}} onClick={()=>props.history.push('/admin/ordermanagement_page')} className="card-link">
-                            <div >
-                                <div className="card text-white bg-warning o-hidden h-100">
-                                    <div className="card-body">
-                                        <div className="card-body-icon">
-                                            <i className="fas fa-fw fa-shopping-cart"></i>
-                                        </div>
-                                        <div className="mr-5">
-                                            <a>Quản lý đơn hàng</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                        <a style={{cursor:'pointer'}} onClick={()=>props.history.push('/admin/customer_page')} className="card-link">
-                            <div>
-                                <div className="card text-white bg-success o-hidden h-100">
-                                    <div className="card-body">
-                                        <div className="card-body-icon">
-                                            <i className="fas fa-fw fa-list"></i>
-                                        </div>
-                                        <div className="mr-5">
-                                            <a>Quản lý khách hàng</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div> */}
                 </div>
             </div>
             <div style={{ paddingTop: '180px' }}><Footer /></div>
